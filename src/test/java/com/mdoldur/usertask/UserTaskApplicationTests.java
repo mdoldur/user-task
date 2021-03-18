c9d6a925348688f2a18bacc53d51da21c817c70f
Mehmet D <mdoldur@hotmail.com>
2021-03-18 23:52:35 +0300

    duzeltme java versiyon
    duzeltme java versiyon

---
+++ src/test/java/com/mdoldur/usertask/UserTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdoldur.usertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
